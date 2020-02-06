8fc3e94ac4e8933f40763d3776a76dd0a530e214
wang shuwen <wangshuwen@yit.com>
2020-02-06 21:39:50 +0800

    irregular-drop-shadows

--- src/app/chapter4/irregular-drop-shadows/irregular-drop-shadows.component.ts
+++ src/app/chapter4/irregular-drop-shadows/irregular-drop-shadows.component.ts
@@ -5,12 +5,15 @@ import style from './irregular-drop-shadows.component.less'
   selector: 'app-irregular-drop-shadows',
   template: `
     <div class="container">
-      <div class="cube bubble">bubble</div>
-      <div class="cube cube2"></div>
-      <div class="cube cube3"></div>
-      <div class="cube cube4"></div>
-      <div class="cube cube5"></div>
-      <div class="cube cube6"></div>
+      <div class="cube bubble shadow">bubble</div>
+      <div class="cube dotted-border shadow">Dotted Border</div>
+      <div class="cube cutout-corners shadow">Cutout Corners</div>
+      <div class="cube clip-path shadow">Clip Path</div>
+
+      <div class="cube bubble filter-shadow">bubble</div>
+      <div class="cube dotted-border filter-shadow">Dotted Border</div>
+      <div class="cube cutout-corners filter-shadow">Cutout Corners</div>
+      <div class="cube clip-path filter-shadow">Clip Path</div>
     </div>
     <app-highlight-code [code]="code"></app-highlight-code>
   `,
